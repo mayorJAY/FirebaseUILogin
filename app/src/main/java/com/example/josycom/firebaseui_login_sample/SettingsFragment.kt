e4214b436f029fa8fb85e8d603e906a72a11e833
mayorjay <joseolu4gsm@yahoo.com>
2020-05-11 01:27:13 +0100

    Initial commit

---
+++ app/src/main/java/com/example/josycom/firebaseui_login_sample/SettingsFragment.kt
@@ -0,0 +1,24 @@
+package com.example.josycom.firebaseui_login_sample
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    companion object {
+        const val TAG = "SettingsFragment"
+    }
+
+    // Get a reference to the ViewModel scoped to this Fragment
+    private val viewModel by viewModels<LoginViewModel>()
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.settings, rootKey)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
